22bf78034959b1d76b4ce5479d42c10acc8198b6
Duy <duy@scondoo.de>
2019-12-08 14:14:56 +0100

    Init food-manager

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+app.get('/order/:orderId', function (req, res) {
+    res.send('Get order by Id');
+});
+
+app.get('/article/:articleId', function (req, res) {
+    res.send('Get article by Id');
+});
+
+app.route('/order')
+    .post(function (req, res) {
+        res.send('Create order');
+    })
+    .put(function (req, res) {
+        res.send('Update order');
+    })
+    .delete(function (req, res) {
+        res.send('Delete order');
+    });
+
+app.route('/article')
+    .post(function (req, res) {
+        res.send('Create article');
+    })
+    .put(function (req, res) {
+        res.send('Update article');
+    })
+    .delete(function (req, res) {
+        res.send('Delete article');
+    });
+
+app.route('/worker')
+    .get(function (req, res) {
+        res.send('Get current active order');
+    })
+    .put(function (req, res) {
+        res.send('Next order');
+    });
+
+app.route('/manager')
+    .get(function (req, res) {
+        res.send('Get all active orders');
+    })
+    .put(function (req, res) {
+        res.send('Prioritize take away orders');
+    });
+
+app.listen(port, () => console.log(`Food Manager listening on port ${port}!`));
